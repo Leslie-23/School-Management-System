e0f56d4b306b3ba6e132c7bff5235cb91c732ed8
leslie-23 <leslieajayi27@gmail.com>
2024-09-12 18:45:43 +0000

    fixing CORS

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,21 @@ const dotenv = require("dotenv");
 // const bodyParser = require("body-parser")
 const app = express();
 const Routes = require("./routes/route.js");
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-dotenv.config();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://paltech-school-management-system.netlify.app/",
+    ],
+    credentials: true,
+    exposedHeaders: ["X-Total-Count"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
 
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
@@ -25,7 +36,6 @@ mongoose
   .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
 app.use("/", Routes);
-
 app.get("/", (req, res) => {
   res.send("API is running");
 });
